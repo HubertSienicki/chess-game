c0adb38523404c56442f8e537501153e1eab6070
HubertSienicki <businesssienicki@gmail.com>
2022-02-18 00:56:46 +0000

    Added a board and pieces

---
+++ ChessEngine.py
@@ -0,0 +1,23 @@
+import pygame
+import os
+from pygame.locals import *
+import sys
+
+
+class GameState:
+    def __init__(self):
+        #The definition of a gamestate, "--" means an empty space of the board for the ease of parsing
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
+
+    
--- main.py
+++ main.py
@@ -1,14 +1,60 @@
-from chess_Board import GameScreen
+import ChessEngine
+import pygame as py
 
+WIDTH = HEIGHT = 512 #Window sizings
+DIMENSION = 8 #Chessboard dimensions
+SQUARESIZE = HEIGHT // DIMENSION
+MAX_FPS = 30
+IMAGES = {}
 
+#Loading pieces 
+#The pieces can be accesed using an id
+def LoadImages():
+    pieces = ['wP','bP','wN','bN','wR','bR','wB','bB','wK','bK','wQ','bQ']
+    for piece in pieces:
+        IMAGES[piece] = py.transform.scale(py.image.load("images/" + piece + ".png"), (SQUARESIZE,SQUARESIZE))
 
 def main():
-    game = GameScreen()
-    game.startGame()
+    py.init()
+    screen = py.display.set_mode((WIDTH, HEIGHT))
+    clock = py.time.Clock()
+    screen.fill(py.Color("white"))
+    gs = ChessEngine.GameState()
+    LoadImages()
+    running = True
 
+    while running:
+        for e in py.event.get():
+            if e.type == py.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        py.display.flip()
 
+#creating an instance of the game
+def drawGameState(screen, gs):
+    #draw the board
+    drawBoard(screen)
+    #draw pieces
+    drawPieces(screen, gs.board)
 
+#Draw the board
+def drawBoard(screen):
+    #Colors of the squares on the board
+    colors = [py.Color("white"), py.Color("grey")]
 
+    for x in range(DIMENSION):
+        for y in range(DIMENSION):
+            color = colors[((x+y)%2)]
+            py.draw.rect(screen, color, py.Rect(y*SQUARESIZE, x*SQUARESIZE, SQUARESIZE, SQUARESIZE))
 
-if __name__ == '__main__':
+#Draw pieces using the current gamestate
+def drawPieces(screen, board):
+    for x in range(DIMENSION):
+        for y in range(DIMENSION):
+            piece = board[x][y]
+            if piece != "--":   #Not an empty square
+                screen.blit(IMAGES[piece], py.Rect(y*SQUARESIZE, x*SQUARESIZE, SQUARESIZE, SQUARESIZE))
+        
+if __name__ == "__main__":
     main()
